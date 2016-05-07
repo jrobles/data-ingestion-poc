b65a7f1f91232b9e471b4d26af05d4a42873c5aa
josemrobles <joserobles@robi.local>
2016-05-06 23:47:42 -0400

    tired

--- api/main.go
+++ api/main.go
@@ -8,6 +8,6 @@ import (
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("api/v1/csv", csvImport).Methods("POST")
+	router.HandleFunc("/api/v1/csv", csvImport).Methods("POST")
 	log.Fatal(http.ListenAndServe(":1337", router))
 }
